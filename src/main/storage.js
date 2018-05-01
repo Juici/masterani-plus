a6869d95ccb5191366013c760b528f9b6066f8ce
James Whaley <juicy66173@gmail.com>
2018-05-01 18:46:10 +0100

    Add storage module

---
+++ src/main/storage.js
@@ -0,0 +1,37 @@
+/**
+ * A utility module for interfacing with the userscript storage.
+ */
+const storage = {
+    /**
+     * Remove an item from the storage.
+     *
+     * @param {string} key - The key of the item to remove.
+     */
+    remove(key) {
+        GM_deleteValue(key);
+    },
+
+    /**
+     * Set the value of an item in the storage.
+     *
+     * @param {string} key - The key of the item.
+     * @param {string|boolean|number} value - The value of the item.
+     */
+    set(key, value) {
+        GM_setValue(key, value);
+    },
+
+    /**
+     * Gets the value of an item in the storage.
+     *
+     * @param {string} key - The key of the item.
+     * @param {string|boolean|number} [def] - The default value if the item does not exist.
+     *
+     * @returns {string|boolean|number} The value of the item, the default value, or `null`.
+     */
+    get(key, def) {
+        return GM_getValue(key, def);
+    },
+};
+
+module.exports = storage;
