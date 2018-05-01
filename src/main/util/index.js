cf72d43cc0e2a589b0d7b927fd1e9580214c31d6
James Whaley <juicy66173@gmail.com>
2018-05-01 17:51:54 +0100

    Add docs to util module

--- src/main/util/index.js
+++ src/main/util/index.js
@@ -1,5 +1,11 @@
 const query = require('./query');
 
+/**
+ * Query the document with the given selector.
+ *
+ * @param selector {string} The query selector.
+ * @returns {Promise<HTMLElement>}
+ */
 function q(selector) {
     return query(selector);
 }
