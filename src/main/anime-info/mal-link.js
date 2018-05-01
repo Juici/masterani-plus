0e4c539b2d4c2c604f78f496b54bba87d0680a51
James Whaley <juicy66173@gmail.com>
2018-05-01 19:13:25 +0100

    Cache Masterani id against MAL id

--- src/main/anime-info/mal-link.js
+++ src/main/anime-info/mal-link.js
@@ -1,43 +1,59 @@
 const http = require('../http');
-const info = require('./info');
+const storage = require('../storage');
+const { anime } = require('./info');
 const _ = require('../util');
 
-const search = `https://myanimelist.net/anime.php?q=${encodeURIComponent(info.anime.title)}`;
-const findUrl = /<a[^>]*?class="[^"]*?\bhoverinfo_trigger\b[^"]*?"[^>]*?href="([^"]*?)"[^>]*?>/i;
+const search = `https://myanimelist.net/anime.php?q=${encodeURIComponent(anime.title)}`;
+const result = /<a[^>]*?class="[^"]*?\bhoverinfo_trigger\b[^"]*?"[^>]*?href="https?:\/\/myanimelist\.net\/anime\/(\d+)\/[^"]*?"[^>]*?>/i;
 
-function addLink(url) {
-    _.q('.ui.sections.list').then(sections => {
-        const link = document.createElement('a');
-        link.className = 'item';
-        link.href = url;
-        link.target = '_blank';
-        link.innerText = 'MyAnimeList';
+const linker = {
+    _add(id) {
+        _.q('.ui.sections.list').then(sections => {
+            const link = document.createElement('a');
+            link.className = 'item';
+            link.href = `https://myanimelist.net/anime/${id}`;
+            link.target = '_blank';
+            link.innerText = 'MyAnimeList';
 
-        sections.appendChild(link);
+            sections.appendChild(link);
+        });
+    },
+
+    add(id) {
+        if (document.readyState !== 'loading') {
+            this._add(id);
+        } else {
+            document.addEventListener('DOMContentLoaded', () => this._add(id));
+        }
+    },
+
+    request() {
+        return http.request({
+            url: search,
+            method: 'GET',
+        }).then(res => new Promise((resolve, reject) => {
+            const page = res.responseText;
+            const match = result.exec(page);
+
+            if (match) {
+                resolve(match[1]);
+            } else {
+                reject();
+            }
+        }));
+    },
+};
+
+const cache = JSON.parse(storage.get('cache', '{}'));
+
+let id = cache[anime.id];
+if (id != null) {
+    linker.add(id);
+} else {
+    linker.request().then(id => {
+        cache[anime.id] = id;
+        storage.set('cache', JSON.stringify(cache));
+
+        linker.add(id);
     });
 }
-
-function requestLink() {
-    return http.request({
-        url: search,
-        method: 'GET',
-        timeout: 5000,
-    }).then(res => new Promise((resolve, reject) => {
-        const page = res.responseText;
-        const match = findUrl.exec(page);
-
-        if (match) {
-            resolve(match[1]);
-        } else {
-            reject();
-        }
-    }));
-}
-
-requestLink().then(url => {
-    if (document.readyState !== 'loading') {
-        addLink(url);
-    } else {
-        document.addEventListener('DOMContentLoaded', () => addLink(url));
-    }
-});
